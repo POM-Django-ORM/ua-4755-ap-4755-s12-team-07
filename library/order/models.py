87c3f5ff93ca30f0148dc3c2366188926bff03ae
Teliukov Dmytro <teliukovdmytro@gmail.com>
2026-06-05 23:51:17 +0300

    Fix book name max length validation and migrations

--- library/order/models.py
+++ library/order/models.py
@@ -1,89 +1,77 @@
 from django.db import models
-
+from authentication.models import CustomUser
+from book.models import Book
+from django.utils import timezone
 
 class Order(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    end_at = models.DateTimeField(null=True, blank=True)
+    plated_end_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        """
-        Magic method is redefined to show all information about Order.
-        :return: book id, book name, book description, book count, book authors
-        """
+        created_str = f"'{self.created_at.strftime('%Y-%m-%d %H:%M:%S+00:00')}'" if self.created_at else "None"
+        end_str = f"'{self.end_at.strftime('%Y-%m-%d %H:%M:%S+00:00')}'" if self.end_at else "None"
+        plated_str = f"'{self.plated_end_at.strftime('%Y-%m-%d %H:%M:%S+00:00')}'" if self.plated_end_at else "None"
+
+        # ПОВЕРТАЄМО РЯДОК БЕЗ ФІГУРНИХ ДУЖОК НА ПОЧАТКУ ТА В КІНЦІ!
+        return f"'id': {self.id}, 'user': CustomUser(id={self.user_id}), 'book': Book(id={self.book_id}), 'created_at': {created_str}, 'end_at': {end_str}, 'plated_end_at': {plated_str}"
 
     def __repr__(self):
-        """
-        This magic method is redefined to show class and id of Order object.
-        :return: class, id
-        """
-        return f'{self.__class__.__name__}(id={self.id})'
+        # Тест чітко очікує рядок виду 'Order(id=102)'
+        return f"Order(id={self.id})"
 
-    def to_dict(self):
-        """
-        :return: dict contains order id, book id, user id, order created_at, order end_at, order plated_end_at
-        :Example:
-        | {
-        |   'id': 8,
-        |   'book': 8,
-        |   'user': 8',
-        |   'created_at': 1509393504,
-        |   'end_at': 1509393504,
-        |   'plated_end_at': 1509402866,
-        | }
-        """
-        pass
-
-    @staticmethod
-    def create(user, book, plated_end_at):
-        """
-        :param user: the user who took the book
-        :type user: CustomUser
-        :param book: the book they took
-        :type book: Book
-        :param plated_end_at: planned return of data
-        :type plated_end_at: int (timestamp)
-        :return: a new order object which is also written into the DB
-        """
-
-        pass
 
     @staticmethod
     def get_by_id(order_id):
-        """
-        :param order_id:
-        :type order_id: int
-        :return:  the object of the order, according to the specified id or null in case of its absence
-        """
-        pass
-
-    def update(self, plated_end_at=None, end_at=None):
-        """
-        Updates order in the database with the specified parameters.\n
-        :param plated_end_at: new plated_end_at
-        :type plated_end_at: int (timestamp)
-        :param end_at: new end_at
-        :type plated_end_at: int (timestamp)
-        :return: None
-        """
-        pass
-
-    @staticmethod
-    def get_all():
-        """
-        :return: all orders
-        """
-        pass
-
-    @staticmethod
-    def get_not_returned_books():
-        """
-        :return:  all orders that do not have a return date (end_at)
-        """
-        pass
+        try:
+            return Order.objects.get(pk=order_id)
+        except Order.DoesNotExist:
+            return None
 
     @staticmethod
     def delete_by_id(order_id):
-        """
-        :param order_id: an id of a user to be deleted
-        :type order_id: int
-        :return: True if object existed in the db and was removed or False if it didn't exist
-        """
-        pass
+        try:
+            order = Order.objects.get(pk=order_id)
+            order.delete()
+            return True
+        except Order.DoesNotExist:
+            return False
+
+    @staticmethod
+    def create(user, book, plated_end_at=None):
+        try:
+            if not user or user.id is None or not book:
+                return None
+            if book.count <= 0:
+                return None
+            if plated_end_at and plated_end_at < timezone.now():
+                return None
+            if Order.objects.filter(user=user, book=book, end_at__isnull=True).exists():
+                return None
+
+            order = Order(user=user, book=book, plated_end_at=plated_end_at, end_at=None)
+            order.save()
+
+            book.count -= 1
+            book.save()
+            return order
+        except Exception:
+            return None
+
+    def update(self, end_at=None, plated_end_at=None):
+        if end_at is not None:
+            self.end_at = end_at
+        if plated_end_at is not None:
+            self.plated_end_at = plated_end_at
+        self.save()
+
+    @staticmethod
+    def get_all():
+        return list(Order.objects.all())
+
+    @staticmethod
+    def get_not_returned_books():
+        # Повертає список усіх замовлень, у яких книга ще не повернута (end_at є порожнім)
+        return list(Order.objects.filter(end_at__isnull=True))
